c03590c21c1bef5b82f526c27220c5acf32d50f3
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-17 20:53:38 -0500

    added more scriptjs code for fetching data

--- script.js
+++ script.js
@@ -39,14 +39,21 @@ window.addEventlistener("load", function() {
      }); 
  });
 
- /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+//  This block of code shows how to format the HTML once you fetch some planetary JSON!
+ fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   response.json().then( function(json) {
+      const div = document.getElementById("missionTarget");
+
+      div.innerHTML = `
+   <h2>Mission Destination</h2>
+   <ol>
+      <li>Name: ${json.name}</li>
+      <li>Diameter: ${json.diameter}</li>
+      <li>Star: ${json.star}</li>
+      <li>Distance from Earth: ${json.distance}</li>
+      <li>Number of Moons: ${json.moons}</li>
+   </ol>
+   <img src="${json.image}">`
+   });
+});
+
