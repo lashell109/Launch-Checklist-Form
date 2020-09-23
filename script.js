36a6b4d7a32744e46331ac052bab4d6ce25d14c7
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-22 20:51:50 -0500

    Fixed some code to get it working

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Write your JavaScript code here!
-window.addEventlistener("load", function() {
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+const divTarget = document.getElementById("missionTarget");
+
+   divTarget.innerHTML = `
+      <h2>Mission Destination</h2>
+      <ol>
+         <li>Name: ${json[3].name}</li>
+         <li>Diameter: ${json[3].diameter}</li>
+         <li>Star: ${json[3].star}</li>
+         <li>Distance from Earth: ${json[3].distance}</li>
+         <li>Number of Moons: ${json[3].moons}</li>
+      </ol>
+      <img src="${json[3].image}"> `
+      });
+   });
+
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       event.preventDefault();
@@ -18,44 +35,30 @@ window.addEventlistener("load", function() {
      if(pilotName === "" || copilotName === "" || fuelLevel === "" || isNaN(fuelLevel) || cargoMass === "" || isNaN(cargoMass)) {
          alert("All fields are required!"); } 
 
-         else {
+      else {
             document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName}`
             document.getElementById("copilotStatus").innerHTML = `CoPilot ${copilotName}`
 
             if (fuelLevel < 10000) {
-               fuelStatus = ("There is not enough fuel for the journey"); }
+               launchStatus.style.color = "red";
+               fuelStatus.innerHTML = "There is not enough fuel for the journey"; }
             
-               else {
-               fuelStatus = ("Fuel level high enough for launch");
+            else {
+               launchStatus.style.color = "green";
+               fuelStatus.innerHTML = "Fuel level high enough for launch";
             }
-                  if (cargoMass > 10000) {
-                     cargoStatus = ("Too much mass for the shuttle to take off"); }
+
+            if (cargoMass > 10000) {
+               launchStatus.style.color = "red";
+               cargoStatus.innerHTML = "Too much mass for the shuttle to take off"; }
             
-                     else {
-                        cargoMass = ("Cargo mass low enough for launch");
+            else {
+               launchStatus.style.color = "green";
+               cargoMass = ("Cargo mass low enough for launch");
          }
      }
   }); 
 });
 
 //  This block of code shows how to format the HTML once you fetch some planetary JSON!
-function fetchData() {
 
-fetch("https://handlers.education.launchcode.org/static/planets.json")
-   .then(function(response) {
-      response.json().then(function(json) {
-const div = document.getElementById("missionTarget");
-
-   div.innerHTML = `
-      <h2>Mission Destination</h2>
-      <ol>
-         <li>Name: ${json.name}</li>
-         <li>Diameter: ${json.diameter}</li>
-         <li>Star: ${json.star}</li>
-         <li>Distance from Earth: ${json.distance}</li>
-         <li>Number of Moons: ${json.moons}</li>
-      </ol>
-      <img src="${json.image}"> `
-      });
-   });
-}
